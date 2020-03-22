c1a77453b3f05379984daf72589f5a3a06e7272d
calvinchankf <chan9118kin@gmail.com>
2020-03-22 14:53:48 +0800

    add template

---
+++ template.py
@@ -0,0 +1,18 @@
+"""
+    1st approach: hashtable
+
+    Time    
+    Space   
+"""
+def f():
+    pass
+
+# input() reads a string with a line of input, stripping the ' ' (newline) at the end.
+# This is all you need for most Code Jam problems.
+t = int(input())  # read a line with a single integer
+for i in range(1, t + 1):
+    arr = [int(s) for s in raw_input().split(" ")]
+    k = int(raw_input())
+    res = f(arr, k)
+
+    print("Case #{}: {}".format(i, res))
